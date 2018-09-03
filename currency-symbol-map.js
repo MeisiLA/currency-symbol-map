b6136ba411adc9da3294575476b2ea62f79ca23e
Lukas Ameisbichler <Lukas.Ameisbichler@tractive.com>
2018-09-03 14:15:16 +0200

    Changed default to currencyCode which was inserted, changed some symbols according to the wording guidelines/conventions

--- currency-symbol-map.js
+++ currency-symbol-map.js
@@ -1,9 +1,9 @@
 var currencySymbolMap = require('./map')
 
 module.exports = function getSymbolFromCurrency (currencyCode) {
-  if (typeof currencyCode !== 'string') return undefined
+  if (typeof currencyCode !== 'string') return currencyCode
   var code = currencyCode.toUpperCase()
-  if (!currencySymbolMap.hasOwnProperty(code)) return undefined
+  if (!currencySymbolMap.hasOwnProperty(code)) return currencyCode
   return currencySymbolMap[code]
 }
 
